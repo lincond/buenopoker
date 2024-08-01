0d7eb31b61f0d418cb64cac203c01aa882009bbc
Lincon Dias <lincond@protonmail.com>
2024-08-01 01:26:24 -0300

    feat: configura liquidjs

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,29 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { Liquid } from 'liquidjs';
+import * as path from 'node:path';
+import { NestExpressApplication } from '@nestjs/platform-express';
+import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create<NestExpressApplication>(AppModule);
+  const engine = new Liquid({
+    layouts: path.join(process.cwd(), 'views', 'layouts'),
+  });
+
+  engine.registerFilter('BRL', (value) =>
+    parseFloat(value).toLocaleString('pt-BR', {
+      style: 'currency',
+      currency: 'BRL',
+    }),
+  );
+  app.engine('liquid', engine.express());
+  app.useStaticAssets(path.join(process.cwd(), 'public'));
+  app.setBaseViewsDir(path.join(process.cwd(), 'views'));
+  app.setViewEngine('liquid');
+
+  app.useGlobalPipes(new ValidationPipe({ transform: true }));
+
   await app.listen(3000);
 }
 bootstrap();
